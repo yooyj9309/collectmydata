ee5a8478b612b7e09a129bfa0862a1cb63538619
WonJoon Choi <wj.choi@rainist.com>
2021-01-19 14:14:13 +0900

    Add submodule schedule (#12)

---
+++ schedule/src/test/java/com/banksalad/collectmydata/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banksalad.collectmydata.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
