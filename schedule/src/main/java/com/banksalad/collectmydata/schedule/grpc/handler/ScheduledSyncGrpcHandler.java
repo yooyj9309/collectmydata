e0612f5dc9a032ca29ea929e569475d9f13fde9a
Junhee Ko <jko@rainist.com>
2021-01-26 09:26:00 +0900

    [MYDA-149] Add ScheduledSyncGrpcHandler (#26)
    * Update idlVersion
    
    * Add ScheduledSyncGrpcHandler with test code

--- schedule/src/main/java/com/banksalad/collectmydata/schedule/grpc/handler/ScheduledSyncGrpcHandler.java
+++ schedule/src/main/java/com/banksalad/collectmydata/schedule/grpc/handler/ScheduledSyncGrpcHandler.java
@@ -4,45 +4,81 @@ import org.springframework.stereotype.Service;
 
 import com.banksalad.collectmydata.schedule.sync.dto.ScheduledSyncRequest;
 import com.banksalad.collectmydata.schedule.sync.schedule.ScheduledSyncScheduler;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.HealthCheckRequest;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.HealthCheckResponse;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.RegisterScheduledSyncRequest;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.RegisterScheduledSyncResponse;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.UnregisterScheduledSyncRequest;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.UnregisterScheduledSyncResponse;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectscheduleGrpc.CollectscheduleImplBase;
+import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+// TODO : - How to validate request parameters
+//        - RegisterScheduledSyncRequest, UnregisterScheduledSyncRequest
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class ScheduledSyncGrpcHandler extends CollectScheduleGrpc.CollectscheduleImplBase implements
-    ScheduledSyncHandler {
+public class ScheduledSyncGrpcHandler extends CollectscheduleImplBase implements ScheduledSyncHandler {
 
   private final ScheduledSyncScheduler scheduledSyncScheduler;
 
-  public void registerScheduledSync(RegisterScheduledSyncRequest request,
-      StreamObserver<RegisterScheduledSyncResponse> responseObserver) {
+  public void registerScheduledSync(
+      RegisterScheduledSyncRequest request,
+      StreamObserver<RegisterScheduledSyncResponse> responseObserver
+  ) {
     try {
       ScheduledSyncRequest scheduledSyncRequest = ScheduledSyncRequest.builder()
-          .banksaladUserId()
-          .orgCode()
-          .organizationId()
+          .banksaladUserId(request.getBanksaladUserId())
+          .sector(request.getSector())
+          .industry(request.getIndustry())
+          .organizationId(request.getOrganizationId())
           .build();
       scheduledSyncScheduler.register(scheduledSyncRequest);
 
+      RegisterScheduledSyncResponse response = RegisterScheduledSyncResponse.newBuilder().build();
       responseObserver.onNext(response);
       responseObserver.onCompleted();
     } catch (Exception e) {
+      log.error("Register Scheduled Sync Error: {}", e.getMessage());
       responseObserver.onError(e);
     }
   }
 
-  public void unregisterScheduledSync(UnregisterScheduledSyncRequest request,
-      StreamObserver<UnregisterScheduledSyncResponse> responseObserver) {
+  public void unregisterScheduledSync(
+      UnregisterScheduledSyncRequest request,
+      StreamObserver<UnregisterScheduledSyncResponse> responseObserver
+  ) {
     try {
       ScheduledSyncRequest scheduledSyncRequest = ScheduledSyncRequest.builder()
-          .banksaladUserId()
-          .orgCode()
-          .organizationId()
+          .banksaladUserId(request.getBanksaladUserId())
+          .sector(request.getSector())
+          .industry(request.getIndustry())
+          .organizationId(request.getOrganizationId())
           .build();
       scheduledSyncScheduler.unregister(scheduledSyncRequest);
 
+      UnregisterScheduledSyncResponse response = UnregisterScheduledSyncResponse.newBuilder().build();
       responseObserver.onNext(response);
       responseObserver.onCompleted();
     } catch (Exception e) {
+      log.error("Unregister Scheduled Sync Error: {}", e.getMessage());
+      responseObserver.onError(e);
+    }
+  }
+
+  @Override
+  public void healthCheck(
+      HealthCheckRequest request,
+      StreamObserver<HealthCheckResponse> responseObserver
+  ) {
+    try {
+      HealthCheckResponse response = HealthCheckResponse.newBuilder().build();
+      responseObserver.onCompleted();
+      responseObserver.onNext(response);
+    } catch (Exception e) {
+      log.error("Health Check Error: {}", e.getMessage());
       responseObserver.onError(e);
     }
   }
---
+++ schedule/src/test/java/com/banksalad/collectmydata/schedule/grpc/handler/ScheduledSyncGrpcHandlerTest.java
@@ -0,0 +1,135 @@
+package com.banksalad.collectmydata.schedule.grpc.handler;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.banksalad.collectmydata.schedule.sync.schedule.ScheduledSyncScheduler;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.HealthCheckRequest;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.HealthCheckResponse;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.RegisterScheduledSyncRequest;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.RegisterScheduledSyncResponse;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.UnregisterScheduledSyncRequest;
+import com.github.banksalad.idl.apis.v1.collectschedule.CollectScheduleProto.UnregisterScheduledSyncResponse;
+import io.grpc.internal.testing.StreamRecorder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.times;
+
+@SpringBootTest
+@DisplayName("ScheduledSyncGrpcHandlerTest")
+class ScheduledSyncGrpcHandlerTest {
+
+  @InjectMocks
+  private ScheduledSyncGrpcHandler scheduledSyncGrpcHandler;
+
+  @Mock
+  private ScheduledSyncScheduler scheduledSyncScheduler;
+
+  @Test
+  @DisplayName("gRPC Call Success Test : registerScheduledSync")
+  void givenRegisterScheduledSyncRequest_whenCalledRegisterScheduledSyncMethod_thenSuccess() {
+    // Given
+    RegisterScheduledSyncRequest registerScheduledSyncRequest = getRegisterScheduledSyncRequest();
+    StreamRecorder<RegisterScheduledSyncResponse> streamRecorder = StreamRecorder.create();
+
+    // When
+    scheduledSyncGrpcHandler.registerScheduledSync(registerScheduledSyncRequest, streamRecorder);
+
+    // Then
+    assertNull(streamRecorder.getError());
+    then(scheduledSyncScheduler).should(times(1)).register(any());
+  }
+
+  @Test
+  @DisplayName("gRPC Call Fail Test : registerScheduledSync")
+  void givenScheduledSyncSchedulerThrowException_whenCalledRegisterScheduledSyncMethod_thenGetError() {
+    // Given
+    RegisterScheduledSyncRequest registerScheduledSyncRequest = getRegisterScheduledSyncRequest();
+    StreamRecorder<RegisterScheduledSyncResponse> streamRecorder = StreamRecorder.create();
+    willThrow(new RuntimeException("Test Message")).given(scheduledSyncScheduler).register(any());
+
+    // When
+    scheduledSyncGrpcHandler.registerScheduledSync(registerScheduledSyncRequest, streamRecorder);
+
+    // Then
+    assertNotNull(streamRecorder.getError());
+    assertEquals(RuntimeException.class, streamRecorder.getError().getClass());
+    assertEquals("Test Message", streamRecorder.getError().getMessage());
+  }
+
+  @Test
+  @DisplayName("gRPC Call Success Test : unregisterScheduledSync")
+  void givenUnregisterScheduledSyncRequest_whenCalledUnregisterScheduledSync_thenResponseSuccess() {
+    // Given
+    UnregisterScheduledSyncRequest registerScheduledSyncRequest = getUnregisterScheduledSyncRequest();
+    StreamRecorder<UnregisterScheduledSyncResponse> streamRecorder = StreamRecorder.create();
+
+    // When
+    scheduledSyncGrpcHandler.unregisterScheduledSync(registerScheduledSyncRequest, streamRecorder);
+
+    // Then
+    assertNull(streamRecorder.getError());
+    then(scheduledSyncScheduler).should(times(1)).unregister(any());
+  }
+
+  @Test
+  @DisplayName("gRPC Call Fail Test : unregisterScheduledSync")
+  void givenScheduledSyncSchedulerThrowException_whenCalledUnregisterScheduledSyncMethod_thenGetError() {
+    // Given
+    UnregisterScheduledSyncRequest unregisterScheduledSyncRequest = getUnregisterScheduledSyncRequest();
+    StreamRecorder<UnregisterScheduledSyncResponse> streamRecorder = StreamRecorder.create();
+    willThrow(new RuntimeException("Test Message")).given(scheduledSyncScheduler).unregister(any());
+
+    // When
+    scheduledSyncGrpcHandler.unregisterScheduledSync(unregisterScheduledSyncRequest, streamRecorder);
+
+    // Then
+    assertNotNull(streamRecorder.getError());
+    assertEquals(RuntimeException.class, streamRecorder.getError().getClass());
+    assertEquals("Test Message", streamRecorder.getError().getMessage());
+  }
+
+  @Test
+  @DisplayName("gRPC Call Success Test : healthCheck")
+  void givenHealthCheckRequest_whenCalledHealthCheckMethod_thenResponseSuccess() {
+    // Given
+    HealthCheckRequest healthCheckRequest = getHealthCheckRequest();
+    StreamRecorder<HealthCheckResponse> streamRecorder = StreamRecorder.create();
+
+    // When
+    scheduledSyncGrpcHandler.healthCheck(healthCheckRequest, streamRecorder);
+
+    // Then
+    assertNull(streamRecorder.getError());
+  }
+
+  private RegisterScheduledSyncRequest getRegisterScheduledSyncRequest() {
+    return RegisterScheduledSyncRequest.newBuilder()
+        .setBanksaladUserId("1234")
+        .setSector("finance")
+        .setIndustry("card")
+        .setOrganizationId("shinhancard")
+        .build();
+  }
+
+  private UnregisterScheduledSyncRequest getUnregisterScheduledSyncRequest() {
+    return UnregisterScheduledSyncRequest.newBuilder()
+        .setBanksaladUserId("1234")
+        .setSector("finance")
+        .setIndustry("card")
+        .setOrganizationId("shinhancard")
+        .build();
+  }
+
+  private HealthCheckRequest getHealthCheckRequest() {
+    return HealthCheckRequest.newBuilder().build();
+  }
+}
