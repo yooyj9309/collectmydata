cd0d9e06092718d4df07f4d0db111448a17be5d3
dusang <dusang@rainist.com>
2021-02-01 10:25:35 +0900

    [MYDA-141]Connect Add deploy,k8s setting  (#43)
    * Add deploy,k8s setting
    
    * Add dumy yaml, fix livenessProbe,readinessProbe url
    
    * Add curl header
    
    * fix curl setting
    
    * rollback header setting
    
    * Add armeria healthcheck
    
    * fix undefined variable

--- connect/src/main/java/com/banksalad/collectmydata/connect/common/config/ArmeriaConfiguration.java
+++ connect/src/main/java/com/banksalad/collectmydata/connect/common/config/ArmeriaConfiguration.java
@@ -5,9 +5,11 @@ import com.banksalad.collectmydata.connect.grpc.handler.ConnectGrpcService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.banksalad.idl.apis.v1.connectmydata.ConnectmydataGrpc;
 import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
 import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.grpc.GrpcService;
+import com.linecorp.armeria.server.healthcheck.HealthCheckService;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
 import com.linecorp.armeria.server.logging.LoggingService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
@@ -32,6 +34,7 @@ public class ArmeriaConfiguration {
               .enableUnframedRequests(true)
               .build());
 
+      serverBuilder.service("/health",HealthCheckService.builder().build());
       serverBuilder.serviceUnder("/docs", new DocService());
     };
   }
