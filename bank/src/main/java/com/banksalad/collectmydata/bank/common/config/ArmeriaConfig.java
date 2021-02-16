8993ebe6c36051e6865841035ab060b6b341f861
Jayden <gwanggi.lee@gmail.com>
2021-02-16 18:15:56 +0900

    Update armeria config for bank module (#76)
    * Update armeria config for bank module
    
    * Remove gradle cache step

--- bank/src/main/java/com/banksalad/collectmydata/bank/common/config/ArmeriaConfig.java
+++ bank/src/main/java/com/banksalad/collectmydata/bank/common/config/ArmeriaConfig.java
@@ -1,31 +1,38 @@
 package com.banksalad.collectmydata.bank.common.config;
 
-import com.banksalad.collectmydata.bank.grpc.handler.CollectmydataBankGrpcService;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.banksalad.collectmydata.bank.grpc.handler.CollectmydataBankGrpcService;
+import com.linecorp.armeria.common.grpc.GrpcMeterIdPrefixFunction;
 import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
 import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.grpc.GrpcService;
+import com.linecorp.armeria.server.healthcheck.HealthCheckService;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
 import com.linecorp.armeria.server.logging.LoggingService;
+import com.linecorp.armeria.server.metric.MetricCollectingService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Configuration
-public class ArmeriaConfiguration {
-
-  public ArmeriaConfiguration(CollectmydataBankGrpcService collectmydataBankGrpcService) {
-    this.collectmydataBankGrpcService = collectmydataBankGrpcService;
-  }
+public class ArmeriaConfig {
 
   private final CollectmydataBankGrpcService collectmydataBankGrpcService;
+  private final MeterRegistry meterRegistry;
+
+  public ArmeriaConfig(CollectmydataBankGrpcService collectmydataBankGrpcService, MeterRegistry meterRegistry) {
+    this.collectmydataBankGrpcService = collectmydataBankGrpcService;
+    this.meterRegistry = meterRegistry;
+  }
 
   @Bean
   public ArmeriaServerConfigurator armeriaServerConfigurator() {
     return serverBuilder -> {
 
       serverBuilder.decorator(LoggingService.newDecorator());
+      serverBuilder.decorator(MetricCollectingService.newDecorator(GrpcMeterIdPrefixFunction.of("grpc.service")));
+
       serverBuilder.accessLogWriter(AccessLogWriter.combined(), false);
 
       serverBuilder.service(
@@ -36,6 +43,9 @@ public class ArmeriaConfiguration {
               .build());
 
       serverBuilder.serviceUnder("/docs", new DocService());
+      serverBuilder.service("/health", HealthCheckService.of());
+
+      serverBuilder.meterRegistry(meterRegistry);
     };
   }
 }
