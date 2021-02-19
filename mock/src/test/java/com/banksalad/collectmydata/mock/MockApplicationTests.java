e0b05d9670b54e68785412c653a331fbb0e12bf5
WonJoon Choi <wj.choi@rainist.com>
2021-02-19 11:01:51 +0900

    [MYDA-300] Add mock server (#77)
    * Add mock server
    
    * Add mock server
    
    * Update BA01_002_multi_page_01.json
    
    * Update BA01_002_multi_page_02.json

---
+++ mock/src/test/java/com/banksalad/collectmydata/mock/MockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banksalad.collectmydata.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
