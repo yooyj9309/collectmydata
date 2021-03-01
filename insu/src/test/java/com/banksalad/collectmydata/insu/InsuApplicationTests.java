7463ec96f6b18d071dcd0427034714da222167bc
WonJoon Choi <wj.choi@rainist.com>
2021-03-01 16:34:36 +0900

    [MYDA-300] Set web-application-type to none for industry consumer (#97)
    * Set web-application-type to none for industry consumer
    
    * Update insu/src/main/resources/application.yml
    
    Co-authored-by: Jayden <gwanggi.lee@gmail.com>
    
    * Set web-application-type to none for industry consumer
    
    Co-authored-by: Jayden <gwanggi.lee@gmail.com>

--- insu/src/test/java/com/banksalad/collectmydata/insu/InsuApplicationTests.java
+++ insu/src/test/java/com/banksalad/collectmydata/insu/InsuApplicationTests.java
@@ -1,13 +1,12 @@
 package com.banksalad.collectmydata.insu;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class InsuApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
