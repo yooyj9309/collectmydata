41fec363a6af7ab112f244d80ca097936924c31b
dusang <dusang@rainist.com>
2021-03-15 19:03:23 +0900

    [MYDA-464][telecom] Add execution, api (#189)
    * Add api, execution
    
    * fix apis, execution

--- telecom/src/main/java/com/banksalad/collectmydata/telecom/collect/Apis.java
+++ telecom/src/main/java/com/banksalad/collectmydata/telecom/collect/Apis.java
@@ -1,8 +1,51 @@
 package com.banksalad.collectmydata.telecom.collect;
 
+import com.banksalad.collectmydata.common.collect.api.Api;
+import com.banksalad.collectmydata.common.collect.api.Pagination;
+
 public class Apis {
 
   private enum HttpMethod {
     GET, POST
   }
+
+  // 6.9.1 통신 계약 목록 조회
+  public static Api finance_telecom_summaries =
+      Api.builder()
+          .id("TC01")
+          .name("TC01-통신 계약 목록 조회")
+          .endpoint("/telecoms?org_code={org_code}&search_timestamp={search_timestamp}")
+          .method(HttpMethod.GET.name())
+          .build();
+
+  // 6.9.2 청구 정보 조회
+  public static Api finance_telecom_bills =
+      Api.builder()
+          .id("TC02")
+          .name("TC02-청구 정보 조회")
+          .endpoint("/telecoms/bills?org_code={org_code}&charge_month={charge_month}")
+          .method(HttpMethod.GET.name())
+          .build();
+
+  // 6.9.3 통신 거래내역 조회
+  public static Api finance_telecom_transactions =
+      Api.builder()
+          .id("TC03")
+          .name("TC03-통신 거래내역 조회")
+          .endpoint("/telecoms/transactions")
+          .method(HttpMethod.POST.name())
+          .build();
+
+  // 6.9.4 결제내역 조회
+  public static Api finance_telecom_paid_transactions =
+      Api.builder()
+          .id("TC04")
+          .name("TC04-결제내역 조회")
+          .endpoint("/telecoms/paid-transactions")
+          .method(HttpMethod.POST.name())
+          .pagination(Pagination.builder()
+              .nextPage("next_page")
+              .build())
+          .build();
+
 }
--- telecom/src/main/java/com/banksalad/collectmydata/telecom/collect/Executions.java
+++ telecom/src/main/java/com/banksalad/collectmydata/telecom/collect/Executions.java
@@ -1,5 +1,38 @@
 package com.banksalad.collectmydata.telecom.collect;
 
+import com.banksalad.collectmydata.common.collect.execution.Execution;
+import com.banksalad.collectmydata.telecom.common.dto.ListTelecomSummaryResponse;
+import com.banksalad.collectmydata.telecom.telecom.dto.ListTelecomBillResponse;
+import com.banksalad.collectmydata.telecom.telecom.dto.ListTelecomPaidTransactionResponse;
+import com.banksalad.collectmydata.telecom.telecom.dto.ListTelecomTransactionResponse;
+
 public class Executions {
 
+  // 6.9.1 통신 계약 목록 조회
+  public static final Execution finance_telecom_summaries =
+      Execution.create()
+          .exchange(Apis.finance_telecom_summaries)
+          .as(ListTelecomSummaryResponse.class)
+          .build();
+
+  // 6.9.2 청구 정보 조회
+  public static Execution finance_telecom_bills =
+      Execution.create()
+          .exchange(Apis.finance_telecom_bills)
+          .as(ListTelecomBillResponse.class)
+          .build();
+
+  // 6.9.3 통신 거래내역 조회
+  public static Execution finance_telecom_transactions =
+      Execution.create()
+          .exchange(Apis.finance_telecom_transactions)
+          .as(ListTelecomTransactionResponse.class)
+          .build();
+
+  // 6.9.4 결제내역 조회
+  public static Execution finance_telecom_paid_transactions =
+      Execution.create()
+          .exchange(Apis.finance_telecom_paid_transactions)
+          .as(ListTelecomPaidTransactionResponse.class)
+          .build();
 }
