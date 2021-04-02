34f3f1fffb890d392e56d2fc0d8e7555b8e0bb54
Junhee Ko <jko@rainist.com>
2021-04-02 11:28:26 +0900

    [MYDA-675] Refactor idl version up (#331)
    * Refactor idl version up
    
    * Comment daas package usage

--- bank/src/main/java/com/banksalad/collectmydata/bank/common/dto/UserSyncStatusResponse.java
+++ bank/src/main/java/com/banksalad/collectmydata/bank/common/dto/UserSyncStatusResponse.java
@@ -1,20 +1,20 @@
-package com.banksalad.collectmydata.bank.common.dto;
-
-import com.github.banksalad.idl.daas.v1.collect.bank.BankProto;
-import lombok.Builder;
-import lombok.Getter;
-
-import java.util.List;
-
-@Builder
-@Getter
-public class UserSyncStatusResponse {
-
-  private final List<UserSyncStatus> userSyncStatuses;
-
-  public BankProto.GetSyncStatusResponse toSyncStatusResponseProto() {
-    return BankProto.GetSyncStatusResponse
-        .newBuilder()
-        .build();
-  }
-}
+//package com.banksalad.collectmydata.bank.common.dto;
+//
+//import com.github.banksalad.idl.daas.v1.collect.bank.BankProto; // TODO : Fix IDL package
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//import java.util.List;
+//
+//@Builder
+//@Getter
+//public class UserSyncStatusResponse {
+//
+//  private final List<UserSyncStatus> userSyncStatuses;
+//
+//  public BankProto.GetSyncStatusResponse toSyncStatusResponseProto() {
+//    return BankProto.GetSyncStatusResponse
+//        .newBuilder()
+//        .build();
+//  }
+//}
