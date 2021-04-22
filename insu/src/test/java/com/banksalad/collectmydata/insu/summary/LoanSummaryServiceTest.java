28f95acf7a15a1c8503bf80bc8f5db4cb72afbb1
Junhee Ko <jko@rainist.com>
2021-04-22 14:26:20 +0900

    [MYDA-642][INSU] Add unit test : insu-008 (#418)
    * Add unit test mock files
    
    * Add unit test : insu-008

--- insu/src/test/java/com/banksalad/collectmydata/insu/summary/LoanSummaryServiceTest.java
+++ insu/src/test/java/com/banksalad/collectmydata/insu/summary/LoanSummaryServiceTest.java
@@ -2,101 +2,158 @@ package com.banksalad.collectmydata.insu.summary;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.wiremock.WireMockSpring;
-import org.springframework.http.HttpStatus;
 
-import com.banksalad.collectmydata.common.collect.execution.ExecutionContext;
 import com.banksalad.collectmydata.finance.api.summary.SummaryRequestHelper;
 import com.banksalad.collectmydata.finance.api.summary.SummaryResponseHelper;
 import com.banksalad.collectmydata.finance.api.summary.SummaryService;
+import com.banksalad.collectmydata.finance.common.db.entity.UserSyncStatusEntity;
+import com.banksalad.collectmydata.finance.common.db.repository.UserSyncStatusRepository;
 import com.banksalad.collectmydata.finance.common.exception.ResponseNotOkException;
-import com.banksalad.collectmydata.insu.collect.Executions;
+import com.banksalad.collectmydata.finance.test.template.dto.TestCase;
 import com.banksalad.collectmydata.insu.common.db.entity.LoanSummaryEntity;
 import com.banksalad.collectmydata.insu.common.db.repository.LoanSummaryRepository;
 import com.banksalad.collectmydata.insu.summary.dto.ListLoanSummariesRequest;
 import com.banksalad.collectmydata.insu.summary.dto.LoanSummary;
+import com.banksalad.collectmydata.insu.template.ServiceTest;
+import com.banksalad.collectmydata.insu.template.provider.LoanSummaryInvocationContextProvider;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import javax.transaction.Transactional;
-import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
 
-import static com.banksalad.collectmydata.insu.common.util.FileUtil.readText;
-import static com.banksalad.collectmydata.insu.common.util.TestHelper.BANKSALAD_USER_ID;
-import static com.banksalad.collectmydata.insu.common.util.TestHelper.ENTITY_IGNORE_FIELD;
-import static com.banksalad.collectmydata.insu.common.util.TestHelper.ORGANIZATION_CODE;
-import static com.banksalad.collectmydata.insu.common.util.TestHelper.ORGANIZATION_ID;
-import static com.banksalad.collectmydata.insu.common.util.TestHelper.getExecutionContext;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.IGNORING_ENTITY_FIELDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-public class LoanSummaryServiceTest {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Transactional
+@DisplayName("보험-008 대출상품 목록 조회")
+public class LoanSummaryServiceTest extends
+    ServiceTest<Object, UserSyncStatusEntity, LoanSummaryEntity, Object> {
 
   @Autowired
-  private SummaryService<ListLoanSummariesRequest, LoanSummary> loanSummaryService;
+  private SummaryService<ListLoanSummariesRequest, LoanSummary> mainService;
   @Autowired
   private SummaryRequestHelper<ListLoanSummariesRequest> requestHelper;
   @Autowired
   private SummaryResponseHelper<LoanSummary> responseHelper;
   @Autowired
-  private LoanSummaryRepository loanSummaryRepository;
-  private static WireMockServer wireMockServer;
+  private UserSyncStatusRepository parentRepository;
+  @Autowired
+  private LoanSummaryRepository mainRepository;
+
+  private static final WireMockServer wireMockServer = new WireMockServer(WireMockSpring.options().dynamicPort());
 
   @BeforeAll
-  static void setup() {
-    wireMockServer = new WireMockServer(WireMockSpring.options().dynamicPort());
+  static void setUp() {
+
     wireMockServer.start();
-    setupMockServer();
   }
 
   @AfterAll
-  static void tearDown() {
-    wireMockServer.shutdown();
+  static void shutDown() {
+
+    wireMockServer.stop();
   }
 
-  @Test
-  @Transactional
-  @DisplayName("6.5.8 대출 목록정보 조회 테스트 : 성공 케이스")
-  public void getLoanSummariesTest_case1() throws ResponseNotOkException {
-    ExecutionContext context = getExecutionContext(wireMockServer.port());
-    loanSummaryService
-        .listAccountSummaries(context, Executions.insurance_get_loan_summaries, requestHelper, responseHelper);
+  @AfterEach
+  void tearDown() {
 
-    List<LoanSummaryEntity> loanSummaryEntities = loanSummaryRepository.findAll();
-    assertEquals(1, loanSummaryEntities.size());
-    assertThat(loanSummaryEntities.get(0)).usingRecursiveComparison()
-        .ignoringFields(ENTITY_IGNORE_FIELD)
-        .isEqualTo(
-            LoanSummaryEntity.builder()
-                .banksaladUserId(BANKSALAD_USER_ID)
-                .organizationId(ORGANIZATION_ID)
-                .prodName("보금자리론")
-                .accountNum("123456789")
-                .consent(true)
-                .accountType("3245")
-                .accountStatus("01")
-                .build()
-        );
+    wireMockServer.resetAll();
   }
 
-  private static void setupMockServer() {
-    // 6.5.8 대출 목록 조회
-    wireMockServer.stubFor(get(urlMatching("/loans.*"))
-        .withQueryParam("org_code", equalTo(ORGANIZATION_CODE))
-        .withQueryParam("search_timestamp", equalTo("0"))
-        .willReturn(
-            aResponse()
-                .withStatus(HttpStatus.OK.value())
-                .withHeader("Content-Type", ContentType.APPLICATION_JSON.toString())
-                .withBody(readText("classpath:mock/response/IS11_001_single_page_00.json"))));
+  @TestTemplate
+  @ExtendWith(LoanSummaryInvocationContextProvider.class)
+  public void unitTests(TestCase<Object, UserSyncStatusEntity, LoanSummaryEntity, Object> testCase)
+      throws ResponseNotOkException {
+
+    prepare(testCase, wireMockServer);
+
+    final Integer status = testCase.getExpectedResponses().get(testCase.getExpectedResponses().size() - 1).getStatus();
+
+    if (status != null && status != 200) { // if (mainService instanceof SummaryService)
+      runAndTestException(testCase);
+    } else {
+      runMainService(testCase);
+    }
+    validate(testCase);
+  }
+
+  @Override
+  protected void saveGParents(List<Object> objects) {
+
+  }
+
+  @Override
+  protected void saveParents(List<UserSyncStatusEntity> userSyncStatusEntities) {
+
+    userSyncStatusEntities
+        .forEach(userSyncStatusEntity -> parentRepository.save(userSyncStatusEntity.toBuilder().build()));
+  }
+
+  @Override
+  protected void saveMains(List<LoanSummaryEntity> loanSummaryEntities) {
+
+    loanSummaryEntities
+        .forEach(loanSummaryEntity -> mainRepository.save(loanSummaryEntity.toBuilder().build()));
+  }
+
+  @Override
+  protected void saveChildren(List<Object> objects) {
+
+  }
+
+  @Override
+  protected void runMainService(TestCase<Object, UserSyncStatusEntity, LoanSummaryEntity, Object> testCase)
+      throws ResponseNotOkException {
+
+    mainService
+        .listAccountSummaries(testCase.getExecutionContext(), testCase.getExecution(), requestHelper, responseHelper);
+  }
+
+  @Override
+  protected void validateParents(List<UserSyncStatusEntity> expectedParents) {
+
+    final List<UserSyncStatusEntity> actualParents = parentRepository.findAll();
+
+    assertAll("*** Parent 확인 ***",
+        () -> assertEquals(expectedParents.size(), actualParents.size()),
+        () -> {
+          for (int i = 0; i < expectedParents.size(); i++) {
+            assertThat(actualParents.get(i)).usingRecursiveComparison().ignoringFields(IGNORING_ENTITY_FIELDS)
+                .isEqualTo(expectedParents.get(i));
+          }
+        }
+    );
+  }
+
+  @Override
+  protected void validateMains(List<LoanSummaryEntity> expectedMains) {
+
+    final List<LoanSummaryEntity> actualMains = mainRepository.findAll();
+
+    assertAll("*** Main 확인 ***",
+        () -> assertEquals(expectedMains.size(), actualMains.size()),
+        () -> {
+          for (int i = 0; i < expectedMains.size(); i++) {
+            assertThat(actualMains.get(i)).usingRecursiveComparison().ignoringFields(IGNORING_ENTITY_FIELDS)
+                .isEqualTo(expectedMains.get(i));
+          }
+        }
+    );
+  }
+
+  @Override
+  protected void validateChildren(List<Object> expectedChildren) {
+
   }
 }
---
+++ insu/src/test/java/com/banksalad/collectmydata/insu/template/provider/LoanSummaryInvocationContextProvider.java
@@ -0,0 +1,117 @@
+package com.banksalad.collectmydata.insu.template.provider;
+
+import com.banksalad.collectmydata.common.collect.execution.Execution;
+import com.banksalad.collectmydata.finance.common.db.entity.UserSyncStatusEntity;
+import com.banksalad.collectmydata.finance.test.template.dto.TestCase;
+import com.banksalad.collectmydata.insu.collect.Executions;
+import com.banksalad.collectmydata.insu.common.db.entity.LoanSummaryEntity;
+import com.banksalad.collectmydata.insu.template.testcase.LoanSummaryTestCaseGenerator;
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.BANKSALAD_USER_ID;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.NEW_SYNCED_AT;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.NEW_USS_ST;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.OLD_SYNCED_AT;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.OLD_USS_ST;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.ORGANIZATION_ID;
+
+public class LoanSummaryInvocationContextProvider implements TestTemplateInvocationContextProvider {
+
+  private static final Execution execution = Executions.insurance_get_loan_summaries;
+
+  @Override
+  public boolean supportsTestTemplate(ExtensionContext context) {
+    return true;
+  }
+
+  @Override
+  public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(ExtensionContext context) {
+
+    UserSyncStatusEntity parent1 = UserSyncStatusEntity.builder()
+        .syncedAt(OLD_SYNCED_AT)
+        .banksaladUserId(BANKSALAD_USER_ID)
+        .organizationId(ORGANIZATION_ID)
+        .apiId(execution.getApi().getId())
+        .searchTimestamp(OLD_USS_ST)
+        .build();
+    Map<String, UserSyncStatusEntity> parentMap = Map.of(
+        "parent1", parent1,
+        "newParent1", parent1.toBuilder().syncedAt(NEW_SYNCED_AT).searchTimestamp(OLD_USS_ST).build(),
+        "touchedParent1", parent1.toBuilder().syncedAt(NEW_SYNCED_AT).searchTimestamp(OLD_USS_ST).build(),
+        "updatedParent1", parent1.toBuilder().syncedAt(NEW_SYNCED_AT).searchTimestamp(NEW_USS_ST).build()
+    );
+
+    LoanSummaryEntity main1 = LoanSummaryEntity.builder()
+        .syncedAt(OLD_SYNCED_AT)
+        .banksaladUserId(BANKSALAD_USER_ID)
+        .organizationId(ORGANIZATION_ID)
+        .accountNum("accountNum01")
+        .consent(true)
+        .prodName("01")
+        .accountType("01")
+        .accountStatus("01")
+        .build();
+
+    LoanSummaryEntity main2 = LoanSummaryEntity.builder()
+        .syncedAt(OLD_SYNCED_AT)
+        .banksaladUserId(BANKSALAD_USER_ID)
+        .organizationId(ORGANIZATION_ID)
+        .accountNum("accountNum02")
+        .consent(true)
+        .prodName("02")
+        .accountType("02")
+        .accountStatus("02")
+        .build();
+
+    Map<String, LoanSummaryEntity> mainMap = Map.of(
+        "main1", main1,
+        "updatedMain1", main1.toBuilder().syncedAt(NEW_SYNCED_AT).prodName("02").build(),
+        "newMain1", main1.toBuilder().syncedAt(NEW_SYNCED_AT).build(),
+        "newMain2", main2.toBuilder().syncedAt(NEW_SYNCED_AT).build()
+    );
+
+    LoanSummaryTestCaseGenerator<Object, UserSyncStatusEntity, LoanSummaryEntity, Object> generator =
+        new LoanSummaryTestCaseGenerator<>(execution, null, parentMap, mainMap, null);
+
+    return generator.generate().stream().map(this::invocationContext);
+  }
+
+  private TestTemplateInvocationContext invocationContext(
+      TestCase<Object, UserSyncStatusEntity, LoanSummaryEntity, Object> testCase) {
+
+    return new TestTemplateInvocationContext() {
+      @Override
+      public String getDisplayName(int invocationIndex) {
+        return testCase.getDisplayName();
+      }
+
+      @Override
+      public List<Extension> getAdditionalExtensions() {
+        return Collections.singletonList(new ParameterResolver() {
+          @Override
+          public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+              throws ParameterResolutionException {
+            return parameterContext.getParameter().getType().equals(TestCase.class);
+          }
+
+          @Override
+          public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+              throws ParameterResolutionException {
+            return testCase;
+          }
+        });
+      }
+    };
+  }
+}
