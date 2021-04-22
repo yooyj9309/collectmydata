2d12728f7d04a8a3f7e2579c9ebec8b3314da196
drevispas <yonggeon.kim@gmail.com>
2021-04-22 19:08:05 +0900

    [MYDA-625][card] Unit test: CD21 /cards/bills (#390)
    * [MYDA-625][card] Unit test: CD21 /cards/bills
    
    * 200을 STATUS_OK 상수로 대체
    
    * remote와 충돌 해결
    - BillEntity.chargeDay 타입 변경에 따라 수정
    - TestCase 이름 변경

---
+++ card/src/test/java/com/banksalad/collectmydata/card/template/provider/BillBasicInvocationContextProvider.java
@@ -0,0 +1,113 @@
+package com.banksalad.collectmydata.card.template.provider;
+
+import com.banksalad.collectmydata.card.collect.Executions;
+import com.banksalad.collectmydata.card.common.db.entity.BillEntity;
+import com.banksalad.collectmydata.card.template.testcase.BillBasicTestCaseGenerator;
+import com.banksalad.collectmydata.common.collect.execution.Execution;
+import com.banksalad.collectmydata.finance.test.template.dto.TestCase;
+
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static com.banksalad.collectmydata.common.util.NumberUtil.bigDecimalOf;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.BANKSALAD_USER_ID;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.NEW_SYNCED_AT;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.OLD_SYNCED_AT;
+import static com.banksalad.collectmydata.finance.test.constant.FinanceTestConstants.ORGANIZATION_ID;
+
+public class BillBasicInvocationContextProvider implements TestTemplateInvocationContextProvider {
+
+  private static final Execution exeuciton = Executions.finance_card_bills;
+
+  @Override
+  public boolean supportsTestTemplate(ExtensionContext context) {
+    return true;
+  }
+
+  @Override
+  public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(ExtensionContext context) {
+
+    BillEntity main1 = BillEntity.builder()
+        .syncedAt(OLD_SYNCED_AT)
+        .banksaladUserId(BANKSALAD_USER_ID)
+        .organizationId(ORGANIZATION_ID)
+        .chargeAmt(bigDecimalOf(100000, 3))
+        .chargeDay((byte) 14)
+        .chargeMonth(202103)
+        .paidOutDate("20210314")
+        .cardType("01")
+        .build();
+    BillEntity main2 = BillEntity.builder()
+        .syncedAt(OLD_SYNCED_AT)
+        .banksaladUserId(BANKSALAD_USER_ID)
+        .organizationId(ORGANIZATION_ID)
+        .chargeAmt(bigDecimalOf(120000, 3))
+        .chargeDay((byte) 14)
+        .chargeMonth(202102)
+        .paidOutDate("20210214")
+        .cardType("01")
+        .build();
+    BillEntity main3 = BillEntity.builder()
+        .syncedAt(OLD_SYNCED_AT)
+        .banksaladUserId(BANKSALAD_USER_ID)
+        .organizationId(ORGANIZATION_ID)
+        .chargeAmt(bigDecimalOf(500, 3))
+        .chargeDay((byte) 15)
+        .chargeMonth(202103)
+        .paidOutDate("20210317")
+        .cardType("02")
+        .build();
+    Map<String, BillEntity> mainMap = Map.of(
+        "main1", main1,
+        "updatedMain1", main1.toBuilder().syncedAt(NEW_SYNCED_AT).chargeAmt(bigDecimalOf(100001, 3)).build(),
+        "newMain1", main1.toBuilder().syncedAt(NEW_SYNCED_AT).build(),
+        "newMain2", main2.toBuilder().syncedAt(NEW_SYNCED_AT).build(),
+        "newMain3", main3.toBuilder().syncedAt(NEW_SYNCED_AT).build()
+    );
+
+    BillBasicTestCaseGenerator<Object, Object, BillEntity, Object> generator =
+        new BillBasicTestCaseGenerator<>(exeuciton, null, null, mainMap, null);
+
+    return generator.generate().stream()
+//        .peek(o -> cprint("Generated testCase",o))
+        .map(this::invocationContext);
+  }
+
+  private TestTemplateInvocationContext invocationContext(
+      TestCase<Object, Object, BillEntity, Object> testCase) {
+
+    return new TestTemplateInvocationContext() {
+      @Override
+      public String getDisplayName(int invocationIndex) {
+        return testCase.getDisplayName();
+      }
+
+      @Override
+      public List<Extension> getAdditionalExtensions() {
+        return Collections.singletonList(new ParameterResolver() {
+          @Override
+          public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+              throws ParameterResolutionException {
+            return parameterContext.getParameter().getType().equals(TestCase.class);
+          }
+
+          @Override
+          public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+              throws ParameterResolutionException {
+            return testCase;
+          }
+        });
+      }
+    };
+  }
+}
