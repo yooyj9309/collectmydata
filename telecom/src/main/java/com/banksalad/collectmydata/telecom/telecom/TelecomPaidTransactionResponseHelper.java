c168492c54c8522ef38ab143ce0ea87f58c128a5
drevispas <yonggeon.kim@gmail.com>
2021-04-26 11:41:34 +0900

    [MYDA-833][telecom] JPA Transactional 추가 (#436)
    TelecomPaidTransactionResponseHelper.saveTransactions()에 @Transactional 추가

--- telecom/src/main/java/com/banksalad/collectmydata/telecom/telecom/TelecomPaidTransactionResponseHelper.java
+++ telecom/src/main/java/com/banksalad/collectmydata/telecom/telecom/TelecomPaidTransactionResponseHelper.java
@@ -14,6 +14,7 @@ import com.banksalad.collectmydata.telecom.telecom.dto.TelecomPaidTransaction;
 
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 
@@ -39,6 +40,7 @@ public class TelecomPaidTransactionResponseHelper implements
     return response.getTransList();
   }
 
+  @Transactional
   @Override
   public void saveTransactions(ExecutionContext executionContext, TelecomSummary telecomSummary,
       List<TelecomPaidTransaction> telecomPaidTransactions) {
