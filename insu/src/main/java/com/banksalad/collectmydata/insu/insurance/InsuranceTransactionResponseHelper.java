bb5c71a7b850950347a565787d2c53370763362f
Junhee Ko <jko@rainist.com>
2021-04-29 18:05:30 +0900

    [MYDA-819][insu] Fix insurance transaction response (#458)
    * Comment test case 007 temporarily
    
    * Fix transaction response process
    
    * Remove 007. 기존 1건 + 변경 1건 test case
    
    * Refactor entity merge into if body

--- insu/src/main/java/com/banksalad/collectmydata/insu/insurance/InsuranceTransactionResponseHelper.java
+++ insu/src/main/java/com/banksalad/collectmydata/insu/insurance/InsuranceTransactionResponseHelper.java
@@ -3,7 +3,6 @@ package com.banksalad.collectmydata.insu.insurance;
 import org.springframework.stereotype.Component;
 
 import com.banksalad.collectmydata.common.collect.execution.ExecutionContext;
-import com.banksalad.collectmydata.common.util.ObjectComparator;
 import com.banksalad.collectmydata.finance.api.transaction.TransactionResponseHelper;
 import com.banksalad.collectmydata.finance.api.transaction.dto.TransactionResponse;
 import com.banksalad.collectmydata.insu.common.db.entity.InsuranceTransactionEntity;
@@ -18,8 +17,6 @@ import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
-import static com.banksalad.collectmydata.finance.common.constant.FinanceConstant.ENTITY_EXCLUDE_FIELD;
 
 @Component
 @RequiredArgsConstructor
@@ -41,20 +38,16 @@ public class InsuranceTransactionResponseHelper implements
   @Override
   public void saveTransactions(ExecutionContext executionContext, InsuranceSummary insuranceSummary,
       List<InsuranceTransaction> insuranceTransactions) {
-    long banksaladUserId = executionContext.getBanksaladUserId();
-    String organizationId = executionContext.getOrganizationId();
-    String insuNum = insuranceSummary.getInsuNum();
 
     for (InsuranceTransaction insuranceTransaction : insuranceTransactions) {
-      insuranceTransaction.setInsuNum(insuNum);
-      Integer transactionYearMonth = Integer.parseInt(insuranceTransaction.getTransAppliedMonth());
+      insuranceTransaction.setInsuNum(insuranceSummary.getInsuNum());
 
-      InsuranceTransactionEntity transactionEntity = InsuranceTransactionEntity.builder()
-          .transactionYearMonth(transactionYearMonth)
+      InsuranceTransactionEntity insuranceTransactionEntity = InsuranceTransactionEntity.builder()
+          .transactionYearMonth(Integer.parseInt(insuranceTransaction.getTransAppliedMonth()))
           .syncedAt(executionContext.getSyncStartedAt())
-          .banksaladUserId(banksaladUserId)
-          .organizationId(organizationId)
-          .insuNum(insuNum)
+          .banksaladUserId(executionContext.getBanksaladUserId())
+          .organizationId(executionContext.getOrganizationId())
+          .insuNum(insuranceTransaction.getInsuNum())
           .transNo(insuranceTransaction.getTransNo())
           .transDate(insuranceTransaction.getTransDate())
           .transAppliedMonth(Integer.valueOf(insuranceTransaction.getTransAppliedMonth()))
@@ -63,27 +56,18 @@ public class InsuranceTransactionResponseHelper implements
           .payMethod(insuranceTransaction.getPayMethod())
           .build();
 
-      // load existing insurance transaction
       InsuranceTransactionEntity existingTransactionEntity = insuranceTransactionRepository
           .findByBanksaladUserIdAndOrganizationIdAndInsuNumAndTransNoAndTransactionYearMonth(
-              banksaladUserId,
-              organizationId,
-              insuNum,
-              insuranceTransaction.getTransNo(),
-              transactionYearMonth
-          ).orElse(InsuranceTransactionEntity.builder().build());
+              insuranceTransactionEntity.getBanksaladUserId(),
+              insuranceTransactionEntity.getOrganizationId(),
+              insuranceTransactionEntity.getInsuNum(),
+              insuranceTransactionEntity.getTransNo(),
+              insuranceTransactionEntity.getTransactionYearMonth()
+          ).orElse(null);
 
-      // merge
-      insuranceTransactionMapper.merge(transactionEntity, transactionEntity);
-
-      // copy PK for update
-      if (existingTransactionEntity != null) {
-        transactionEntity.setId(existingTransactionEntity.getId());
-      }
-
-      // upsert
-      if (!ObjectComparator.isSame(transactionEntity, existingTransactionEntity, ENTITY_EXCLUDE_FIELD)) {
-        insuranceTransactionRepository.save(transactionEntity);
+      if (existingTransactionEntity == null) {
+        insuranceTransactionMapper.merge(insuranceTransactionEntity, insuranceTransactionEntity);
+        insuranceTransactionRepository.save(insuranceTransactionEntity);
       }
     }
   }
