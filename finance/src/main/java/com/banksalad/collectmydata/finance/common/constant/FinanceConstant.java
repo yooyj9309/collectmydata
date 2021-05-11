468eeec8787c5055b0aeef6597874e98b90c91d9
Hyunjun <74221090+hyunjun1659@users.noreply.github.com>
2021-05-11 14:48:31 +0900

    Add consentId to Entity_exclude_field (#549)

--- finance/src/main/java/com/banksalad/collectmydata/finance/common/constant/FinanceConstant.java
+++ finance/src/main/java/com/banksalad/collectmydata/finance/common/constant/FinanceConstant.java
@@ -4,7 +4,7 @@ public class FinanceConstant {
 
   public static final String AUTHORIZATION = "Authorization";
 
-  public static final String[] ENTITY_EXCLUDE_FIELD = {"syncedAt", "createdAt", "createdBy", "updatedAt", "updatedBy"};
+  public static final String[] ENTITY_EXCLUDE_FIELD = {"syncedAt", "createdAt", "createdBy", "updatedAt", "updatedBy", "consentId", "syncRequestId"};
   public static final String[] INVALID_RESPONSE_CODE = {"40305", "40404"};
 
   public static final int DEFAULT_SEARCH_YEAR = 5;
